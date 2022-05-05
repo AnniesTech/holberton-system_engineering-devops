2e16f2b4e976202d7edf246a6eb18791bd0eb58f
AnniesTech <anita.rocha0505@gmail.com>
2022-05-04 19:37:58 -0500

    readme and task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# Fixes problem of high amount of requests
+exec { 'changeLimit':
+    command => 'sed -i "s/15/4096/g" /etc/default/nginx; service nginx restart',
+    path    => '/usr/bin/:/bin/',
+}
